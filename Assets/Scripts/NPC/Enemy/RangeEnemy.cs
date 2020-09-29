2878fff1bdd5ec836d9ce66e824c6318cb4e4d50
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2020-09-29 20:16:13 +0200

    SnadToFunguje

---
+++ Assets/Scripts/NPC/Enemy/RangeEnemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeEnemy : MonoBehaviour {
+
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private Transform player;
+
+    private float timeBtwShots;
+    public float startTimeBtwWShots;
+    public GameObject projecitile;
+    public Transform firePoint;
+    public float bulletForce = 20f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwWShots;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+
+
+        if(timeBtwShots <= 0)
+        {
+            Shoot();
+            timeBtwShots = startTimeBtwWShots; 
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+	
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(projecitile, transform.position, Quaternion.identity);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
